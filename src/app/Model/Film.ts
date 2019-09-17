8b790fcc0e31786e1df6d9fba8d025174e34f424
Raul-Coelho <raulcc50@gmail.com>
2019-09-16 21:37:06 -0300

    Modelando Entidade Film

---
+++ src/app/Model/Film.ts
@@ -0,0 +1,19 @@
+export class Film{
+    constructor(
+        public characters: string[],
+        public created: string,
+        public director: string,
+        public edited: string,
+        public episode_id: string,
+        public opening_crawl: string,
+        public planets: string[],
+        public producer: string,
+        public release_date:string,
+        public species: string[],
+        public starships: string[],
+        public title: string,
+        public url: string,
+        public vehicles: string[],
+
+    ){}
+}
