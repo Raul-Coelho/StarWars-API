ce903a32db12d309f5b3c9da53b2af0576c5b8b3
Raul-Coelho <raulcc50@gmail.com>
2019-09-19 11:14:23 -0300

    Configurações visuais para os cards de espaçonaves

--- src/app/pages/detail-film/detail-film.component.ts
+++ src/app/pages/detail-film/detail-film.component.ts
@@ -7,6 +7,10 @@ import { ConsumeCharactersService } from '../../services/consume-characters.serv
 import { People } from '../../Model/People';
 import { ConsumePlanetsService } from '../../services/consume-planets.service';
 import { Planet } from '../../Model/Planet';
+import { ConsumeSpeciesService } from '../../services/consume-species.service';
+import { Specie } from '../../Model/Specie';
+import { ConsumeStarshipsService } from '../../services/consume-starships.service';
+import { Starships } from '../../Model/Starships';
 
 @Component({
   selector: 'app-detail-film',
@@ -15,41 +19,60 @@ import { Planet } from '../../Model/Planet';
 })
 export class DetailFilmComponent implements OnInit {
 
-  public film:Film;
-  public characters:People[];
+  public film: Film;
+  public characters: People[];
   public planets: Planet[];
+  public species: Specie[];
+  public starships: Starships[];
   public headers: Headers;
-  public nav:any;
+  public nav: any;
 
   constructor(
-    private consumePlanetsService:ConsumePlanetsService,
     private consume: ConsumeService,
-    private consumeCharactersService:ConsumeCharactersService, 
-    private http:HttpClient,
+    private consumePlanetsService: ConsumePlanetsService,
+    private consumeCharactersService: ConsumeCharactersService,
+    private consumeSpeciesService: ConsumeSpeciesService,
+    private consumeStarshipsService: ConsumeStarshipsService,
+    private http: HttpClient,
     private router: Router,
-    ) 
-    
-    { 
+  ) {
     this.headers = new Headers();
     this.headers.append('Content-Type', 'application/json');
 
     // RECEBENDO FILME
     this.nav = this.router.getCurrentNavigation();
-    
+
   }
-  
+
 
   ngOnInit() {
     this.film = this.nav.extras.state.selectedFilm;
-    this.consumeCharactersService.getCharacters(this.film['characters']).then( res =>{
+    this.consumeCharactersService.getCharacters(this.film['characters']).then(res => {
       this.characters = res;
       console.log(this.characters)
+    }).catch(res =>{
+      console.log
     })
 
-    console.log(this.consumePlanetsService.getPlanets(this.film['planets']).then(res =>{
+    this.consumePlanetsService.getPlanets(this.film['planets']).then(res => {
       this.planets = res;
       console.log(this.planets)
-    }))
-  }
+    }).catch(res =>{
+      console.log
+    })
 
+    this.consumeSpeciesService.getSpecies(this.film['species']).then(res =>{
+      this.species = res;
+      console.log(this.species)
+    }).catch(res =>{
+      console.log
+    })
+
+    this.consumeStarshipsService.getStarships(this.film['starships']).then(res =>{
+      this.starships = res;
+      console.log(this.starships);
+    }).catch(res =>{
+      console.log
+    })
+  }
 }
