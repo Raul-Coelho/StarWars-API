8c1eda880e64aa719e4c86c291a81d5029837f9f
Raul-Coelho <raulcc50@gmail.com>
2019-09-19 08:48:30 -0300

    Criando Classe Modelo para Planeta

---
+++ src/app/Model/Planets.ts
@@ -0,0 +1,25 @@
+export class People{
+    constructor(
+        
+        public name: string,
+        public rotation_period: string,
+        public orbital_period: string,
+        public diameter: string,
+        public climate: string,
+        public gravity: string,
+        public terrain: string,
+        public surface_water: string,
+        public population: string,
+        public residents: string[],
+        public films:string[],
+        public created: string,
+        public edited: string[],
+        public vehicles: string[],
+
+
+
+
+
+
+    ){}
+}
