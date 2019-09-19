1ed83097554d61e9dbbca98728c2194b14ceca0e
Raul-Coelho <raulcc50@gmail.com>
2019-09-19 11:13:45 -0300

    Implementando service de Espaçonaves

--- src/app/services/consume-starships.service.ts
+++ src/app/services/consume-starships.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Starships } from '../Model/Starships';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsumeStarshipsService {
 
-  constructor() { }
+  public starship:Starships[];
+
+  private readonly API = `${environment.API}/starships/`;
+
+  private NEXT = `${environment.API_CHARAC}`;
+
+  constructor(private http:HttpClient) { }
+
+  async getStarships(starshipsArray:string[]){
+    this.starship = [];
+    starshipsArray.forEach(async element => {
+      this.NEXT = element
+      await this.http.get<Starships>(this.NEXT).toPromise().then(retorno =>{
+        this.starship.push(retorno);
+      }).catch(erro =>{
+        console.log
+      })
+    });
+    return this.starship;
+  }
 }
