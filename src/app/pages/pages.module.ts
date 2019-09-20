96b3605829e685c7226425db6c314eb82f8bf6fb
Raul-Coelho <raulcc50@gmail.com>
2019-09-20 10:32:11 -0300

    Integrando componentização dos cards

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,19 +6,20 @@ import { ListCardComponent } from './list-card/list-card.component';
 import { MatListModule } from '@angular/material/list';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppModule } from '../app.module';
-
-
-
-
+import { CardCharactersComponent } from '../components/card-characters/card-characters.component';
+import { CardPlanetsComponent } from '../components/card-planets/card-planets.component';
+import { CardSpeciesComponent } from '../components/card-species/card-species.component';
+import { CardVehiclesComponent } from '../components/card-vehicles/card-vehicles.component';
+import { CardStarshipsComponent } from '../components/card-starships/card-starships.component';
 
 
 
 const routes: Routes = [
   {
-    path: 'detail/film', component:DetailFilmComponent
+    path: 'detail/film', component: DetailFilmComponent
   },
   {
-    path:'', component:ListCardComponent
+    path: '', component: ListCardComponent
   }
 ]
 
@@ -26,10 +27,15 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     DetailFilmComponent,
+    CardCharactersComponent,
+    CardPlanetsComponent,
+    CardSpeciesComponent,
+    CardVehiclesComponent,
+    CardStarshipsComponent,
 
   ],
   imports: [
-    CommonModule,
+  CommonModule,
     RouterModule,
     MatListModule,
     MDBBootstrapModule.forRoot(),
